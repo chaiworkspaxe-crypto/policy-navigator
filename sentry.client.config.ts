918e6ada0c512240e57bb1579bf5d2b292aca373
chaiworkspaxe-crypto <chaiworkspaxe@gmail.com>
2026-04-28 22:55:57 +0900

    Update sentry.client.config.ts

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,7 +1,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN || process.env.SENTRY_DSN,
-  tracesSampleRate: 1.0, 
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  tracesSampleRate: 1.0, // client는 사용량 적으니 1.0 유지
   debug: false,
 });
